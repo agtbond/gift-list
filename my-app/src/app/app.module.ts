acc0469203b074d42d905d63ad38b18f0692512a
Agata <blumf@poczta.onet.pl>
2018-08-22 23:22:08 +0200

    new componenets

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -3,12 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ContactComponent } from './view/contact/contact.component';
+import { AddListComponent } from './view/add-list/add-list.component';
+import { FindListComponent } from './view/find-list/find-list.component';
+import { HomeComponent } from './view/home/home.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ContactComponent
+    ContactComponent,
+    AddListComponent,
+    FindListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
